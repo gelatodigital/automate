f6f72c2451d1249944d1bbaa9e1042a94c2263bd
Hilmar X <x@hilmarx.com>
2021-08-01 01:32:01 +0200

    new matic deployments

---
+++ deploy/TaskTreasuryMatic.deploy.js
@@ -0,0 +1,33 @@
+const { sleep } = require("@gelatonetwork/core");
+
+module.exports = async (hre) => {
+  if (
+    hre.network.name === "mainnet" ||
+    hre.network.name === "rinkeby" ||
+    hre.network.name === "ropsten"
+  ) {
+    console.log(
+      `Deploying TaskTreasuryMatic to ${hre.network.name}. Hit ctrl + c to abort`
+    );
+    await sleep(10000);
+  }
+
+  const { deployments } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await hre.getNamedAccounts();
+
+  await deploy("TaskTreasuryMatic", {
+    from: deployer,
+    args: [hre.network.config.GELATO],
+  });
+};
+
+module.exports.skip = async (hre) => {
+  const skip =
+    hre.network.name === "mainnet" ||
+    hre.network.name === "rinkeby" ||
+    hre.network.name === "hardhat"; // skip local deployment here for tests to run
+  return skip ? true : false;
+};
+
+module.exports.tags = ["TaskTreasuryMatic"];
