9cb69288bac229d54d6439cfaa852612609f7eb3
Brandon Chuah <songlin.chuah@gmail.com>
2022-03-28 11:42:31 +0800

    feat: deploy on gnosis chain

--- hardhat/config/addresses.ts
+++ hardhat/config/addresses.ts
@@ -10,6 +10,7 @@ export const getGelatoAddress = (network: string): string | undefined => {
   const GELATO_AVALANCHE = "0x7C5c4Af1618220C090A6863175de47afb20fa9Df";
   const GELATO_ARBITRUM = "0x4775aF8FEf4809fE10bf05867d2b038a4b5B2146";
   const GELATO_BSC = "0x7C5c4Af1618220C090A6863175de47afb20fa9Df";
+  const GELATO_GNOSIS = "0x29b6603D17B9D8f021EcB8845B6FD06E1Adf89DE";
 
   switch (network) {
     case "mainnet":
@@ -30,7 +31,11 @@ export const getGelatoAddress = (network: string): string | undefined => {
       return GELATO_ARBITRUM;
     case "bsc":
       return GELATO_BSC;
+    case "gnosis":
+      return GELATO_GNOSIS;
     case "hardhat":
       return GELATO_MAINNET;
+    default:
+      throw new Error("Gelato contract address not found");
   }
 };
