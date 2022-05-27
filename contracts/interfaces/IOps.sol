5193880e710c2cc1d9c71964f8c9aef14c4a6cc1
Brandon Chuah <songlin.chuah@gmail.com>
2022-05-27 16:24:35 +0800

    chore: refactor ops events

--- contracts/interfaces/IOps.sol
+++ contracts/interfaces/IOps.sol
@@ -3,34 +3,6 @@ pragma solidity ^0.8.0;
 import {ITaskTreasuryUpgradable} from "./ITaskTreasuryUpgradable.sol";
 
 interface IOps {
-    /// @notice Events ///
-
-    event ExecSuccess(
-        uint256 indexed txFee,
-        address indexed feeToken,
-        address indexed execAddress,
-        bytes execData,
-        bytes32 taskId,
-        bool callSuccess
-    );
-    event TaskCreated(
-        address taskCreator,
-        address execAddress,
-        bytes4 selector,
-        address resolverAddress,
-        bytes32 taskId,
-        bytes resolverData,
-        bool useTaskTreasuryFunds,
-        address feeToken,
-        bytes32 resolverHash
-    );
-    event TaskCancelled(bytes32 taskId, address taskCreator);
-    event TimerSet(
-        bytes32 indexed taskId,
-        uint128 indexed nextExec,
-        uint128 indexed interval
-    );
-
     /// @notice External functions ///
 
     function createTask(
