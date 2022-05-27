51af152671544499a0e07e52e9312bfdb7f94f72
Brandon Chuah <songlin.chuah@gmail.com>
2022-05-27 15:25:11 +0800

    chore: refactor ops storage

---
+++ contracts/OpsStorage.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+import {
+    EnumerableSet
+} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {LibDataTypes} from "./libraries/LibDataTypes.sol";
+
+// solhint-disable max-states-count
+abstract contract OpsStorage {
+    mapping(bytes32 => address) public taskCreator;
+    mapping(bytes32 => address) public execAddresses;
+    mapping(address => EnumerableSet.Bytes32Set) internal _createdTasks;
+
+    uint256 public fee;
+    address public feeToken;
+
+    // Appended State
+    mapping(bytes32 => LibDataTypes.Time) public timedTask;
+}
