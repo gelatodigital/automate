48801e0c247f61222e5c5ae494ffd546027d4c67
Brandon Chuah <songlin.chuah@gmail.com>
2022-05-27 16:24:35 +0800

    chore: refactor ops events

---
+++ contracts/libraries/LibEvents.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.12;
+
+library LibEvents {
+    event TaskCreated(
+        address taskCreator,
+        address execAddress,
+        bytes4 selector,
+        address resolverAddress,
+        bytes32 taskId,
+        bytes resolverData,
+        bool useTaskTreasuryFunds,
+        address feeToken,
+        bytes32 resolverHash
+    );
+    event TaskCancelled(bytes32 taskId, address taskCreator);
+    event ExecSuccess(
+        uint256 indexed txFee,
+        address indexed feeToken,
+        address indexed execAddress,
+        bytes execData,
+        bytes32 taskId,
+        bool callSuccess
+    );
+    event TimerSet(
+        bytes32 indexed taskId,
+        uint128 indexed nextExec,
+        uint128 indexed interval
+    );
+}
