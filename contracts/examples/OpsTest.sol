d64e773e6fb498f7024a3afa0accdf39c8e9ec7f
Brandon Chuah <songlin.chuah@gmail.com>
2022-06-15 11:51:31 +0800

    style: license

---
+++ contracts/examples/OpsTest.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {OpsReady} from "../vendor/gelato/OpsReady.sol";
+import {IOps} from "../interfaces/IOps.sol";
+
+// solhint-disable no-empty-blocks
+contract OpsTest is OpsReady {
+    uint256 public count;
+
+    constructor(address _ops) OpsReady(_ops) {}
+
+    receive() external payable {}
+
+    function increaseCount(uint256 _count) external {
+        count += _count;
+    }
+
+    function increaseCountNoPrepayment(uint256 _count) external {
+        count += _count;
+
+        uint256 fee;
+        address feeToken;
+
+        (fee, feeToken) = IOps(ops).getFeeDetails();
+
+        _transfer(fee, feeToken);
+    }
+
+    function checker(uint256 _count)
+        external
+        pure
+        returns (bool canExec, bytes memory payload)
+    {
+        canExec = true;
+        payload = abi.encodeWithSelector(this.increaseCount.selector, _count);
+    }
+}
