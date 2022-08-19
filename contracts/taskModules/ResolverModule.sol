736042a320f520be770d991abb030f781d6f946d
Brandon Chuah <songlin.chuah@gmail.com>
2022-08-19 10:28:19 +0200

    chore: remove unused imports

--- contracts/taskModules/ResolverModule.sol
+++ contracts/taskModules/ResolverModule.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.12;
 
 import {TaskModuleBase} from "./TaskModuleBase.sol";
-import {LibEvents} from "../libraries/LibEvents.sol";
 
 contract ResolverModule is TaskModuleBase {
     /**
