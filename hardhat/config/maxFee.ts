66095337ef1d2f107e68a3c2c91d7302ceccb33a
Hilmar X <x@hilmarx.com>
2022-08-23 18:43:32 +0200

    feat: deploy on ogoerli (#67)

--- hardhat/config/maxFee.ts
+++ hardhat/config/maxFee.ts
@@ -35,6 +35,8 @@ export const getMaxFee = (network: string): BigNumber | undefined => {
       return ethers.utils.parseEther("5");
     case "moonriver":
       return ethers.utils.parseEther("2");
+    case "ogoerli":
+      return ethers.utils.parseEther("0");
     case "okovan":
       return ethers.utils.parseEther("0");
     case "hardhat":
