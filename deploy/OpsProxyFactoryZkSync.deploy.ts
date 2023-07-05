648d11e0656bc30030500e0e6d11490ccb46263d
Brandon Chuah <songlin.chuah@gmail.com>
2023-07-05 12:10:25 +0700

    fix: determineProxyAddress on zksync

---
+++ deploy/OpsProxyFactoryZkSync.deploy.ts
@@ -0,0 +1,49 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { sleep } from "../hardhat/utils";
+import { utils } from "zksync-web3";
+import { bytecode } from "../artifacts-zk/contracts/vendor/proxy/EIP173/EIP173OpsProxy.sol/EIP173OpsProxy.json";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  if (hre.network.name !== "hardhat") {
+    console.log(
+      `Deploying OpsProxyFactoryZkSync to ${hre.network.name}. Hit ctrl + c to abort`
+    );
+    await sleep(10000);
+  }
+
+  const { deployments } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await hre.getNamedAccounts();
+
+  const AUTOMATE = (await hre.ethers.getContract("Automate")).address;
+  const OPSPROXY = (await hre.ethers.getContract("OpsProxy")).address;
+
+  const eip173OpsProxyByteCodeHash =
+    "0x" + Buffer.from(utils.hashBytecode(bytecode)).toString("hex");
+
+  await deploy("OpsProxyFactoryZkSync", {
+    from: deployer,
+    proxy: {
+      owner: deployer,
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: [OPSPROXY],
+        },
+      },
+    },
+    args: [AUTOMATE, eip173OpsProxyByteCodeHash],
+    log: hre.network.name !== "hardhat",
+    gasLimit: 3_000_000,
+  });
+};
+
+export default func;
+
+func.skip = async (hre: HardhatRuntimeEnvironment) => {
+  const shouldSkip = hre.network.name !== "hardhat";
+  return shouldSkip;
+};
+
+func.tags = ["OpsProxyFactoryZkSync"];
