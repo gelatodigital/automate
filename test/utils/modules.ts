6d3ba1d5e38981aba28e5ecebab7995eb688efd0
Muhammed Kadir Yücel <kadir@gelato.digital>
2023-07-27 15:02:33 +0300

    chore: add test utils for TriggerModule

--- test/utils/modules.ts
+++ test/utils/modules.ts
@@ -7,6 +7,7 @@ export enum Module {
   PROXY,
   SINGLE_EXEC,
   WEB3_FUNCTION,
+  TRIGGER,
 }
 
 export type ModuleData = {
@@ -31,6 +32,24 @@ export const encodeTimeArgs = (startTime: number, interval: number): string => {
     ["uint128", "uint128"],
     [startTime, interval]
   );
+
+  return encoded;
+};
+
+export const encodeTimeTriggerArgs = (
+  start: number,
+  interval: number
+): string => {
+  const encoded = ethers.utils.defaultAbiCoder.encode(
+    ["uint128", "uint128"],
+    [start, interval]
+  );
+
+  return encoded;
+};
+
+export const encodeCronTriggerArgs = (expression: string): string => {
+  const encoded = ethers.utils.defaultAbiCoder.encode(["string"], [expression]);
 
   return encoded;
 };
