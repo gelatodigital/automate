287f853b5b93c537cb7fb72dea7ad89359bd96c8
Muhammed Kadir Yücel <kadir@gelato.digital>
2023-08-10 12:33:03 +0300

    fix: trigger module encoding

--- test/utils/modules.ts
+++ test/utils/modules.ts
@@ -15,6 +15,24 @@ export type ModuleData = {
   args: string[];
 };
 
+export enum TriggerType {
+  TIME,
+  CRON,
+}
+
+export type TimeTrigger = {
+  type: TriggerType.TIME;
+  interval: number;
+  start?: number;
+};
+
+export type CronTrigger = {
+  type: TriggerType.CRON;
+  cron: string;
+};
+
+export type TriggerConfig = TimeTrigger | CronTrigger;
+
 export const encodeResolverArgs = (
   resolverAddress: string,
   resolverData: string
@@ -36,20 +54,30 @@ export const encodeTimeArgs = (startTime: number, interval: number): string => {
   return encoded;
 };
 
-export const encodeTimeTriggerArgs = (
-  start: number,
-  interval: number
-): string => {
-  const encoded = ethers.utils.defaultAbiCoder.encode(
-    ["uint128", "uint128"],
-    [start, interval]
-  );
+export const encodeTriggerArgs = (trigger: TriggerConfig): string => {
+  let triggerArgs: string;
 
-  return encoded;
-};
+  if (trigger.type === TriggerType.TIME) {
+    const triggerBytes = ethers.utils.defaultAbiCoder.encode(
+      ["uint128", "uint128"],
+      [trigger.start ?? 0, trigger.interval]
+    );
 
-export const encodeCronTriggerArgs = (expression: string): string => {
-  const encoded = ethers.utils.defaultAbiCoder.encode(["string"], [expression]);
+    triggerArgs = ethers.utils.defaultAbiCoder.encode(
+      ["uint128", "bytes"],
+      [Number(TriggerType.TIME), triggerBytes]
+    );
+  } else {
+    const triggerBytes = ethers.utils.defaultAbiCoder.encode(
+      ["string"],
+      [trigger.cron]
+    );
 
-  return encoded;
+    triggerArgs = ethers.utils.defaultAbiCoder.encode(
+      ["uint8", "bytes"],
+      [Number(TriggerType.CRON), triggerBytes]
+    );
+  }
+
+  return triggerArgs;
 };
